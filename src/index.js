ebff5b6fcc9888fc9c8ac5c280109d9281b1dfdb
imy222 <imayyeo@yahoo.com>
2023-07-05 16:41:48 +1000

    Add components and router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import Home from './components/Home.js';
+import About from './components/About.js';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/About",
+        element: <About />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
