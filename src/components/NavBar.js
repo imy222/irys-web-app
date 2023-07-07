260b651e72ec90bf1c3a7cfd9bac0b47e48a1c1a
imy222 <imayyeo@yahoo.com>
2023-07-07 21:32:37 +1000

    Add Navbar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,13 @@ import {Link} from "react-router-dom";
 function NavBar() {
     return (
         <>
-            <Navbar className="top-nav" expand="xl" variant="light" bg="light">
+            <Navbar className="top-nav" expand="xl" variant="light">
                 <Container id="Navbar">
                     <Navbar.Brand href="#home"></Navbar.Brand>
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link className="nav-item" as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link className="nav-item" as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link className="nav-item" as={Link} to="/blog">Blog</Nav.Link>
                         </Nav>
                 </Container>
             </Navbar>
