469a0b87d8f9ea0f662b12512f327cbc72480f13
imy222 <imayyeo@yahoo.com>
2023-07-07 22:13:53 +1000

    Add Card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../App.css';
+import {Card} from './Card';
 
 const Home = () => {
     return (
         <div className="home-container">
             <h1 className="home-title">Irys's World of Wonders</h1>
+            <div className="cards-container">
+                <Card
+                    imgSrc="https://picsum.photos/300/200"
+                    imgAlt="Placeholder image"
+                    title="Card Title"
+                    description="This is the description on the card"
+                    buttonText="Learn More"
+                    link="/blog"
+                />
         </div>
-    )
+        </div>
+    );
 }
 
 export default Home;
